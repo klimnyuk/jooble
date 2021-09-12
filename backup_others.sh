4b1eb4321c37eb38a4305d915e52b41731857151
klimnyuk <87854515+klimnyuk@users.noreply.github.com>
2021-09-12 19:05:03 +0300

    Update backup_others.sh

--- backup_others.sh
+++ backup_others.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+for file in custom.log seo.log error.log chunga.log
+do
+cp $file $file.backup && echo > $file
+sed -i '/127.0.0.1\|192.168.0.100/d' $file.backup
+sed -i -E 's/[a-zA-Z0-9]{32}[0-3][0-9][01][1-9][1,2][0-9][0-9][0-9].*@.* /***** /g' $file.backup
+tar -czf $file$TIMESTAMP.tar $file.backup
+find -name "$file*.tar" -cmin +150 | xargs rm -f
+done
